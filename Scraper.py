a2aa359a8989a66b3048244b8d8f3b4248445440
Samuel Aleks <samuel.j.aleks@gmail.com>
2024-04-12 15:12:33 -0500

    additional json attributes

--- Scraper.py
+++ Scraper.py
@@ -1,15 +1,12 @@
 import re
 import json
-import html
-import httpx
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
 
-
 # Set Firefox options
 options = Options()
-# options.headless = True  # Uncomment this line if you want to run Firefox in headless mode
+options.headless = True  # You can comment/uncomment this line to toggle headless mode
 options.set_preference("browser.userAgent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Firefox/100.0")
 
 # Specify the path to the GeckoDriver executable
@@ -27,20 +24,31 @@ html = driver.page_source
 # Extract data and create a dictionary
 data = re.findall(r'window.mosaic.providerData\["mosaic-provider-jobcards"\]=(\{.+?\});', html)
 data = json.loads(data[0])
-print({
-    "results": data["metaData"]["mosaicProviderJobCardsModel"]["results"],
-    "meta": data["metaData"]["mosaicProviderJobCardsModel"]["tierSummaries"],
-})
+
+# Extract relevant attributes from the results
+jobs = []
+for job in data["metaData"]["mosaicProviderJobCardsModel"]["results"]:
+    job_info = {
+        "title": job.get("title", ""),
+        "searchUID": job.get("searchUID", ""),
+        "truncatedCompany": job.get("truncatedCompany", ""),
+        "jobLocationState": job.get("jobLocationState", ""),
+        "jobLocationCity": job.get("jobLocationCity", ""),
+        "company": job.get("company", ""),
+        "extractedSalary": {
+            "max": job.get("extractedSalary", {}).get("max", ""),
+            "min": job.get("extractedSalary", {}).get("min", ""),
+            "type": job.get("extractedSalary", {}).get("type", "")
+        }   
+    }
+    jobs.append(job_info)
 
 # Close the browser
 driver.quit()
 
-# Convert data to JSON format
-json_data = json.dumps(data, indent=4)
+# Save job data to a JSON file
+with open('jobs.json', 'w') as json_file:
+    json.dump(jobs, json_file, indent=4)
 
-# Save JSON data to a file
-with open('data.json', 'w') as json_file:
-    json_file.write(json_data)
-
-# Print JSON data
-print(json_data)
+# Print the path to the saved JSON data
+print("Job data saved to 'jobs.json'")
